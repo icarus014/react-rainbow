984773cbf0eac2233599d52d135adfa961f70fb5
Darian Brown <browndarian011@gmail.com>
2023-03-18 13:54:11 -0400

    ability to check if input is a real color

--- src/ColorForm.js
+++ src/ColorForm.js
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 
 function ColorForm(props){
     let [input, setInput] = useState('')
+    const isColor = (strColor) => {
+        const s = new Option().style;
+        s.color = strColor;
+        return s.color !== '';
+    }
 
     const handleSubmit = (e) => {
         // We still want to eliminate the default behavior of our form submittal
         e.preventDefault()
         // addColor, the function we just built, should be available within props.
-        props.addColor(input)
+        if (isColor(input)) {
+            props.addColor(input)
+        }
         setInput('')
-
     }
 
     return (
