4a62fe9149b2d6afd2823e6396e1b632c8f7a336
Darian Brown <browndarian011@gmail.com>
2023-03-18 13:48:29 -0400

    cleared text field after submit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, { useState } from 'react'
@@ -7,12 +6,7 @@ import ColorBlock from './ColorBlock'
 import ColorForm from './ColorForm';
 
 function App(){
-    let [colors, setColors] = useState([
-        'violet', 'blue',
-        'lightblue', 'green',
-        'greenyellow', 'yellow',
-        'orange', 'red'
-    ])
+    let [colors, setColors] = useState([])
     
     let colorMap = colors.map((color, i) => {
         return (
--- src/ColorForm.js
+++ src/ColorForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-
 function ColorForm(props){
     let [input, setInput] = useState('')
 
@@ -9,12 +8,14 @@ function ColorForm(props){
         e.preventDefault()
         // addColor, the function we just built, should be available within props.
         props.addColor(input)
+        setInput('')
+
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" 
+                <input type="text" value={input}
                 onChange={(e) => setInput(e.target.value)} />
                 <button type="submit">Submit!</button>
             </form>
